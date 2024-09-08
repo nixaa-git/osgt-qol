c25c5d9187aa178c3ec2e1a47ca2c378b1067a8b
houz <49918237+houzeyhoo@users.noreply.github.com>
2024-09-08 17:13:26 +0200

    .

---
+++ src/dinput8/dinput8.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#define EXPORT __declspec(dllexport)
+
+using DirectInput8Create_t = HRESULT(WINAPI*)(HINSTANCE, DWORD, REFIID, LPVOID*, LPVOID);
+
+// Setup function called in InitializeIfNeeded.
+void Setup() { SetWindowTextA(FindWindowA(NULL, "Growtopia"), "Growtopia [OSGT-QOL]"); }
+
+// Initializes the library if it hasn't been initialized yet.
+void InitializeIfNeeded()
+{
+    static long initialized = 0;
+    if (InterlockedCompareExchange(&initialized, 1, 0) == 0)
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Setup, NULL, 0, NULL);
+}
+
+// Initializes the library if needed and calls original DirectInput8Create.
+EXPORT HRESULT WINAPI DirectInput8Create(HINSTANCE instance, DWORD version, REFIID id, LPVOID* out,
+                                         LPVOID unk)
+{
+    InitializeIfNeeded();
+    // GetSystemDirectoryW(nullptr, 0) will returns the size of the buffer needed to store the path
+    // without the null terminator.
+    uint32_t sysDirPathSize = GetSystemDirectoryW(nullptr, 0);
+    std::wstring path(sysDirPathSize - 1, L'\0');
+    GetSystemDirectoryW(&path[0], sysDirPathSize);
+    path += L"\\dinput8.dll";
+    // Assume dinput8 is on the system, the game depends on it anyway.
+    HMODULE dinput8 = LoadLibraryW(path.c_str());
+    MessageBoxW(NULL, path.c_str(), L"path", MB_OK);
+    auto fn = (DirectInput8Create_t)GetProcAddress(dinput8, "DirectInput8Create");
+    return fn != nullptr ? fn(instance, version, id, out, unk) : E_FAIL;
+}
